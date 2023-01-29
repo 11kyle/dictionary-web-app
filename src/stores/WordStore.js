8742e9471f1fc922044061060cbf8ec983e0a232
11kyle <11kyle.johnson@gmail.com>
2023-01-28 21:27:23 -0600

    Revert "refactored font selection"
    This reverts commit ebb0ed2392e25bed1c481219b2ed06600b489475.

--- src/stores/WordStore.js
+++ src/stores/WordStore.js
@@ -4,6 +4,7 @@ import axios from "axios"
 export const useWordStore = defineStore("word", {
   state: () => ({
     response: null,
+    word: null,
     query: null,
   }),
   getters: {},
@@ -12,11 +13,12 @@ export const useWordStore = defineStore("word", {
       axios
         .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${query}`)
         .then((response) => {
-          // console.log(response)
-          this.response = response // Set state to response
+          console.log(response)
+          this.word = response.data[0] // Set state to response
+          this.response = response
         })
         .catch((error) => {
-          // console.log(error.response)
+          console.log(error.response)
           this.response = error.response // Set state to response
         })
     },
